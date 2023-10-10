47d898fcce991b318924aae2c967a3b7a69fce62
Sean Keery <seke4820@colorado.edu>
2023-10-09 19:32:45 -0600

    playing with imports

--- applications/data-collector/api.py
+++ applications/data-collector/api.py
@@ -1,6 +1,10 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from starlette.status import HTTP_201_CREATED
+# https://github.com/render-examples/fastapi/tree/main
+# docs url will get you an OpenAPI/Swagger rendering
+
+import dbAPI
 
 app = FastAPI()
 
@@ -12,3 +16,8 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/collect/{item_id}",status_code=HTTP_201_CREATED)
+def read_item(item_id: str):
+    dbAPI.create(item_id) 
+    return {"q"}
